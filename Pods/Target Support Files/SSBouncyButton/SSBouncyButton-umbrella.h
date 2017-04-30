7566250d6366ec88816889fb76e42f9c429b10f4
Alex Lee <alexdar94@gmail.com>
2017-04-30 20:55:57 +0800

    .gitignore is now working

--- Pods/Target Support Files/SSBouncyButton/SSBouncyButton-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "SSBouncyButton.h"
-
-FOUNDATION_EXPORT double SSBouncyButtonVersionNumber;
-FOUNDATION_EXPORT const unsigned char SSBouncyButtonVersionString[];
-
